6a93cebe2bf42a08181116a82c17c40e017ac606
smshses1rmup1pjssdj0m0dj01i <77310955+lsmshseslrmuplpjssdjomodjoli@users.noreply.github.com>
2023-07-24 03:40:42 +0000

    Lesson 36 の途中。「完了したTODO」への複製OK

--- src/index.js
+++ src/index.js
@@ -19,11 +19,33 @@ const onClickAdd = () => {
   // (「追加」--07) 「未完了のTODO」に追加する ... <ul> 直下に (<li>として) 登録する
   document.getElementById("incomplete-list").appendChild(li);
   // (「追加」--08) 「完了」「削除」ボタンも生成する
+  // (「完了」--00)
   const buttonComplete = document.createElement("button");
+  // (「完了」--01) 「完了」ボタンが押されたら「完了したTODO」へ作成する
+  buttonComplete.addEventListener("click", (event) => {
+    // (「完了」--02) 「完了」ボタンに該当する「TODO名」を取り出す
+    const completeTarget = event.target.parentNode;
+    // (「完了」--03) 「完了したTODO」に DOM (ul > li) を作成する。
+    const liComplete = document.createElement("li");
+    liComplete.className = "list-row";
+    console.log(`[incomplete -> complete] ${liComplete}`);
+    const spanComplete = document.createElement("span");
+    spanComplete.innerText = completeTarget.firstChild.innerText;
+    liComplete.appendChild(spanComplete);
+    // (「完了」--04) 「TODO」を作成(複製)する
+    document.getElementById("complete-list").appendChild(liComplete);
+    // (「完了」--05) 「戻す」ボタンを作成する
+    const buttonBack = document.createElement("button");
+    buttonBack.addEventListener("click", () => {
+      alert("戻す");
+    });
+    buttonBack.innerText = "戻す";
+    liComplete.appendChild(buttonBack);
+  });
   buttonComplete.innerText = "完了";
+  // (「削除」--00)
   const buttonDelete = document.createElement("button");
   buttonDelete.innerText = "削除";
-
   // (「削除」--01) 「削除」ボタンが押されたら「未完了のTODO」から削除する
   // GPT-4🌀曰く『関数外から「削除」ボタンを識別することはできないので、ボタン作成時にイベントを関連付けること』
   buttonDelete.addEventListener("click", (event) => {
